4cdd30ac1f3071011a1a913e3610c7c59cff2555
WilliamBaldridge <baldridge.william09@gmail.com>
2021-03-22 22:29:21 -0500

    finished printAllContacts and adds addNewContact

--- src/ContactsResourceManager.java
+++ src/ContactsResourceManager.java
@@ -1,18 +1,72 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class ContactsResourceManager {
-  Path p = Paths.get("contacts.txt");
-  List<String> contacts = new ArrayList<>();
+public class ContactsResourceManager extends Contact {
 
-  public List<String> getContacts() {
-    try {
+    private static Path p = Paths.get("src","contacts.txt");
 
-      }
+    private static List<String> contacts = new ArrayList<>();
+
+    public ContactsResourceManager(String firstName, String lastName, String phoneNumber) {
+        super(firstName, lastName, phoneNumber);
     }
 
 
-  }
+    public static void printAllContacts() {
+
+        try {
+
+            contacts = Files.readAllLines(p);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        for (String contact : contacts) {
+
+            System.out.println(contact);
+        }
+
+    }
+
+
+    public void addNewContact() {
+
+        String firstName = super.getFirstName();
+        String lastName = super.getLastName();
+        String phoneNumber = super.getPhoneNumber();
+
+//        userContact Contact(firstName, lastName, phoneNumber);
+
+
+
+//    Contact userContact = new ArrayList<>(Arrays.asList(
+//            firstName,
+//            lastName,
+//            phoneNumber
+//    ));
+
+        try {
+
+            Files.write(p, , StandardOpenOption.APPEND); // append new contact
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+
+
+}
